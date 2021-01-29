7773147aac23ddde37304f26dd5014f67e35708e
shawon <shawonislam121009@gmail.com>
2021-01-29 14:31:15 +0600

    jwt added

--- rest_official_tutorial/settings.py
+++ rest_official_tutorial/settings.py
@@ -44,6 +44,11 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',  # <-- And here
+    ],
+}
 
 
 MIDDLEWARE = [
--- rest_official_tutorial/urls.py
+++ rest_official_tutorial/urls.py
@@ -5,6 +5,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('snippets.urls')),
+    path('jwt/', include('jwt_auth.urls'))
 ]
 urlpatterns += [
     path('api-auth/', include('rest_framework.urls')),
